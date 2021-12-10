e6dd81a2d2928823b4208bd133c7910000eaeb29
Jinbeom Kim <jinbekim@c4r2s3.42seoul.kr>
2021-12-10 19:36:02 +0900

    wp create user

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -5,14 +5,8 @@ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 service mysql start
 
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;"
-# admin user
-mysql -e "CREATE USER '$USER1'@'%' IDENTIFIED BY '$USER1_PASS';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$USER1'@'%';"
-# wordpress user
-mysql -e "CREATE USER '$USER2'@'%' IDENTIFIED BY '$USER2_PASS';"
-mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$USER2'@'%';"
-mysql -e "FLUSH PRIVILEGES;"
+mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASS';"
+mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
 
 # mysqld_safe
 /bin/bash
-# forground로 실행???
