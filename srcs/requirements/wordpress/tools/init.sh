ece5b3b85ea1d236f43d741c93a13d1551dafdff
jinbekim <jinbekim@student.42seoul.kr>
2021-12-10 10:23:45 +0900

    remove wordpress file

---
+++ srcs/requirements/wordpress/tools/init.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+
+# wordpress
+wget -c https://wordpress.org/latest.tar.gz
+tar -xvf latest.tar.gz
+mv wordpress/ /var/www/html
+mv /tmp/wp-config.php /var/www/html/wordpress/
+rm -rf latest.tar.gz && /tmp/wp-config.php
+
+chown -R www-data:www-data /var/www/html
+
+php-fpm7.3 --nodaemonize
