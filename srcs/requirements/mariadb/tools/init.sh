826852004f2449868f27ac0df3b674c8150818d0
jinbekim <jinbekim@student.42seoul.kr>
2021-12-14 15:40:38 +0900

    add shell if condition

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,18 +1,15 @@
 #!bin/bash
 
-#Start MariaDB
-service mysql start
-#Create database
-mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;"
-echo "####Database $DB_NAME created"
-#Create user
-mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PW';"
-mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
-echo "####User $DB_USER created"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PW'; FLUSH PRIVILEGES;"
-echo "####Root password changed"
-mysqladmin -uroot -p$DB_ROOT_PW shutdown
-#End MariaDB
-
+if [ ! -d /var/lib/mysql/$DB_NAME ]; then
+	#Start MariaDB
+	service mysql start
+	#Create database & create user & alter root password & grant privileges
+	mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;\
+		CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PW';\
+		GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';\
+		ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PW'; FLUSH PRIVILEGES;"
+	#End MariaDB
+	mysqladmin -uroot -p$DB_ROOT_PW shutdown
+fi
 #daemon
 exec mysqld_safe
--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,17 +1,25 @@
 #!bin/bash
 
-# To avoid volume conflicts
-mv /wordpress/* /var/www/html/ && rm -rf /wordpress
-chown -R www-data:www-data /var/www/html
+# if empty
+if [ -e /var/www/html/index.nginx-debian.html ]; then
+	# wordpress
+	wget -c https://wordpress.org/latest.tar.gz
+	tar -xvf latest.tar.gz
+	mv /tmp/wp-config.php /wordpress/
+	mv /wordpress/* /var/www/html/
+	chown -R www-data:www-data /var/www/html
+	rm -rf latest.tar.gz /wordpress /var/www/html/index.nginx-debian.html
 
-# install wordpress
-until wp core install --allow-root --url='jinbe.42.fr' --title='WordPress for Inception' --admin_user=$WP_USER1\
- --admin_password=$WP_USER1_PW --admin_email="jin@student.42seoul.kr" --path='/var/www/html'; do
- echo "##### WordPress is unavailable - sleeping"
- sleep 1
- done
+	# install wordpress
+	until wp core install --allow-root --url='jinbe.42.fr' --title='WordPress for Inception' --admin_user=$WP_USER1\
+	--admin_password=$WP_USER1_PW --admin_email="jin@student.42seoul.kr" --path='/var/www/html'; do
+	echo "##### WordPress is unavailable - sleeping"
+	sleep 1
+	done
 
-# create user2
-wp user create --allow-root $WP_USER2 jinbekim@student.42seoul.kr --user_pass=$WP_USER2_PW --role=author --path=/var/www/html
+	# create user2
+	wp user create --allow-root $WP_USER2 jinbekim@student.42seoul.kr --user_pass=$WP_USER2_PW --role=author --path=/var/www/html
+fi
 
+service sendmail start
 php-fpm7.3 --nodaemonize
